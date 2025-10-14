a4f2f59fc476775684a939cec9acc93b173f8b1b
huntyyvera-source <huntyyvera@gmail.com>
2025-10-14 19:23:17 +0800

    三 数据类型学习

---
+++ src/第三章 数据类型.py
@@ -0,0 +1,21 @@
+a = 12.3
+print(type(a))
+# type 查看数据类型s
+
+# Name = input("请输入姓名：")
+# print('你好,%s' % Name)
+# # %s占位符
+
+na = "abc"
+me = 'def'
+name = na + me
+print(na+me)
+print(na*3)
+# 字符串拼接
+
+print(name[0:])
+
+print(len(name))
+
+print(a in name)
+# 成员运算 in 判断是否在字符串里
